e244d4a48dc107d4962473fb306b9d0c571b66c1
venvo <m13434344340@163.com>
2020-06-28 21:12:26 +0800

    [+]fixed

---
+++ test/LongTest.java
@@ -0,0 +1,8 @@
+public class LongTest {
+
+    public static void main(String[] args) {
+        Long a = -128L, b = -128L, c = 150L, d = 150L;
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+}
